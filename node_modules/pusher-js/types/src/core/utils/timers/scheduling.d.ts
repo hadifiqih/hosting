c7245832cad6fa4183668f7543b036014a0e6a03
M. Hadi Fiqih Pratama <40841786+hadifiqih@users.noreply.github.com>
2024-03-29 14:02:09 +0700

    Delete node_modules directory

--- node_modules/pusher-js/types/src/core/utils/timers/scheduling.d.ts
+++
@@ -1,8 +0,0 @@
-interface Scheduler {
-    (TimedCallback: any, number: any): number;
-}
-interface Canceller {
-    (number: any): void;
-}
-type Delay = number;
-export { Scheduler, Canceller, Delay };
